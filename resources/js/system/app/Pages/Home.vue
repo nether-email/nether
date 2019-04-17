2f533dfec34c79ee84964afc7d90d5fdbb471d13
Daniël Klabbers <daniel@klabbers.email>
2019-04-17 21:15:34 +0200

    wip

--- resources/js/system/app/Pages/Home.vue
+++ resources/js/system/app/Pages/Home.vue
@@ -3,12 +3,13 @@
         <section id="hero" class="container w-1/5 justify-center">
             <Logo/>
 
-            <div id="about" class="text-center">
+            <div id="about" class="Card">
+                <p>
                 <strong>Nether</strong>, an open source successor
-                to Google Inbox. Being build in the open, using open source
-                technologies like
-                <a href="https://laravel.com" target="_blank">Laravel</a> and
-                <a href="https://vuejs.org" target="_blank">VueJS</a>.
+                to Google Inbox. Build in the open, using open source
+                technologies like <a href="https://laravel.com" target="_blank">Laravel</a>
+                and <a href="https://vuejs.org" target="_blank">VueJS</a>.
+                </p>
             </div>
         </section>
     </div>
